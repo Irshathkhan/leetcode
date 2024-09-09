f1cffba7a915475cd65916c07a062b1fcee12190
Irshath <115578464+Irshathkhan@users.noreply.github.com>
2024-09-09 23:13:39 +0530

    Create firstUniqChar.java

---
+++ firstUniqChar.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        char[] strTochar = s.toCharArray();
+
+        for (int i = 0; i < strTochar.length; i++) {
+            for (int j = 0; j < strTochar.length; j++) {
+                if (i != j && strTochar[i] == strTochar[j]) {
+                    break;
+                }
+
+                if (j == strTochar.length - 1) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
