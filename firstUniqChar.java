e50a504db86aab963498a0f3d3df390e6bd0b851
Irshath <115578464+Irshathkhan@users.noreply.github.com>
2024-09-09 23:19:32 +0530

    387  . first-unique-character-in-a-string

--- firstUniqChar.java
+++ firstUniqChar.java
@@ -2,7 +2,7 @@ class Solution {
     public int firstUniqChar(String s) {
         char[] strTochar = s.toCharArray();
 
-        for (int i = 0; i < strTochar.length; i++) {
+        for (int i = 0; i < strTochar.length; i++) { 
             for (int j = 0; j < strTochar.length; j++) {
                 if (i != j && strTochar[i] == strTochar[j]) {
                     break;
