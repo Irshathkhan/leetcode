edff6c9434abd78dea55be175424d13f7d2a7197
Irshath <115578464+Irshathkhan@users.noreply.github.com>
2024-09-15 20:57:45 +0530

    Create FindtheHighestAltitude.java

---
+++ FindtheHighestAltitude.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int res = 0;
+        int sum = 0;
+        for(int i=0;i<gain.length;i++){
+            sum = sum + gain[i];
+            if(sum>res){
+                res = sum;
+            }
+        }
+        return res;
+    }
+}
