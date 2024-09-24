d899c9333bbdc40a8022f2f9806652aed4b255c0
Irshath <115578464+Irshathkhan@users.noreply.github.com>
2024-09-24 22:29:37 +0530

    1929. Concatenation of Array

---
+++ 1929. Concatenation of Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int l = nums.length;
+        int[] res = new int[l*2];
+        for(int i = 0; i < l; i++){
+            res[i] = nums[i];
+            res[i+l] = nums[i];
+        }
+        return res;
+    }
+}
